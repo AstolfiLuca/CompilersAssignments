98a54f92fae0dec71dc140a88d4613c53e723b54
Luca Astolfi <316748@studenti.unimore.it>
2025-03-27 19:06:19 +0100

    added new cases to opts 3 of assignments 1

--- assignment1/opts/MultiInstruction.cpp
+++ assignment1/opts/MultiInstruction.cpp
@@ -4,7 +4,7 @@
 
 #include "LocalOpts.h"
 
-bool isOpposite(int opcode, int opcode2){
+bool isOppositeInstr(int opcode, int opcode2){
   return (
     opcode == Instruction::Add && opcode2 == Instruction::Sub ||
     opcode == Instruction::Sub && opcode2 == Instruction::Add ||
@@ -15,35 +15,58 @@ bool isOpposite(int opcode, int opcode2){
 bool runOnBasicBlockOpt3(BasicBlock &BB) {
   for (Instruction &Inst : BB) {
     if(Inst.isBinaryOp()){
-      
       // Instruzione originale
-      auto *originalOp1 = Inst.getOperand(0);
-      auto *originalOp2 = Inst.getOperand(1);
+      int originalOpCode = Inst.getOpcode();  // Codice operazione istruzione originale
+      auto *originalOp1 = Inst.getOperand(0); // 1° Operando
+      auto *originalOp2 = Inst.getOperand(1); // 2° Operando
       
-      auto *originalNumberOp = ConstantInt::get(Inst.getType(), 0); // Operando numero
+      auto *originalNumber = ConstantInt::get(Inst.getType(), 0); // Operando numerico
       Instruction *usedInst; // Operando istruzione (istruzione usata)
+      bool isOriginalFirstOpNumber = true; // Booleano true se il primo operando è un numero, false altrimenti
       
-      if (originalNumberOp = dyn_cast<ConstantInt>(originalOp1)) usedInst = dyn_cast<Instruction>(originalOp2);   
-      else if (originalNumberOp = dyn_cast<ConstantInt>(originalOp2)) usedInst = dyn_cast<Instruction>(originalOp1);
-      if(!usedInst) continue; // Se non c'è un operando istruzione, vado avanti
+      if (originalNumber = dyn_cast<ConstantInt>(originalOp1)) usedInst = dyn_cast<Instruction>(originalOp2);   
+      else if (originalNumber = dyn_cast<ConstantInt>(originalOp2)){
+        usedInst = dyn_cast<Instruction>(originalOp1);      
+        isOriginalFirstOpNumber = false; // 2° Operando
+      }
+      if(!usedInst || !originalNumber) continue; // Se non c'è un operando istruzione, vado avanti
       
+      int originalOpValue = cast<ConstantInt>(originalNumber)->getSExtValue();
+
       // Istruzione usata
+      int usedOpCode = usedInst->getOpcode();
       auto *usedOp1 = usedInst->getOperand(0);
       auto *usedOp2 = usedInst->getOperand(1);
 
-      auto *usedNumberOp = ConstantInt::get(Inst.getType(), 0); 
-      Instruction *usedInstOp;
-      if (usedNumberOp = dyn_cast<ConstantInt>(usedOp1)) usedInstOp = dyn_cast<Instruction>(usedOp2);   
-      else if (usedNumberOp = dyn_cast<ConstantInt>(usedOp2)) usedInstOp = dyn_cast<Instruction>(usedOp1); 
-      
-      
-      // Ottimizzazione su istruzioni opposte
-      if (isOpposite(usedInst->getOpcode(), Inst.getOpcode()) && usedNumberOp == originalNumberOp)
-        Inst.replaceAllUsesWith(usedInstOp);
+      auto *usedNumber = ConstantInt::get(Inst.getType(), 0);
+      Value *usedInstOp; // Tipo Value per comprendere anche i parametri (oltre alle istruzioni)
+      bool isUsedFirstOpNumber = true;
+      if (usedNumber = dyn_cast<ConstantInt>(usedOp1)) usedInstOp = usedOp2;   
+      else if (usedNumber = dyn_cast<ConstantInt>(usedOp2)){
+        usedInstOp = usedOp1; 
+        isUsedFirstOpNumber = false;
+      }
+      if(!usedInstOp || !usedNumber) continue;
 
+      int usedOpValue = cast<ConstantInt>(usedNumber)->getSExtValue();
       
-      // a = b + 1 | c = -1 + a
-      
+      bool optimize = false;
+      if (isOppositeInstr(usedOpCode, originalOpCode) && usedOpValue == originalOpValue){
+        optimize = true;
+        if(originalOpCode == Instruction::Sub && isOriginalFirstOpNumber) optimize = false;
+        if(usedOpCode == Instruction::Sub && isUsedFirstOpNumber) optimize = false;
+      } 
+      else
+        if(usedOpCode == originalOpCode && originalOpCode != Instruction::SDiv){
+          if (originalOpCode == Instruction::Add && (usedOpValue + originalOpValue == 0))  
+            optimize = true;
+          else 
+            if(originalOpCode == Instruction::Sub && usedOpValue == originalOpValue)
+              optimize = true;
+        }
+
+      if(optimize) Inst.replaceAllUsesWith(usedInstOp);
+        
     } // if binary
   } // for
   return true;
--- assignment1/test/cpp/multi_instruction.cpp
+++ assignment1/test/cpp/multi_instruction.cpp
@@ -1,12 +1,71 @@
-int multi_instruction_optimizazion_test(int a, int b){
-    // d = a -> funziona
-    int c = a + 1;
-    int d = c - 1;
-    b = d + 1;
-    //
-    c = 5 + a;
-    d = 5 - c; // d != a --> d = -a (non dovrebbe essere ottimizzato)
-    b = d + 2;
+int multi_instruction_optimizazion_test(int a, int b, int c, int d){
+    // "test" instruction are random instruction needed to prove that d = a, we'll use the number 5 (randomly selected)
+
+    // --- Number at the SECOND operand (2° instruction) ---
+    // ADD - SUB // FUNZIONA
+    c = a + 1;
+    d = c - 1; // d = a + 1 - 1 = a
+    b = d + 5; // test:  
+
+    // ADD (commutative) - SUB // FUNZIONA
+    c = 1 + a;
+    d = c - 1;
+    b = d + 5; // test:
+
+    // SUB - ADD // FUNZIONA
+    c = a - 1;
+    d = c + 1;
+    b = d + 5; // test: d = a - 1 + 1 = a
+
+    // SUB - ADD // DA FARE (instruzione uguale)
+    c = -1 + a;
+    d = c + 1;
+    b = d + 5; // test: d = a - 1 + 1 = a
+
+    // Addizione -> istruzione uguale, operando opposto
+    // Sottrazione -> istruzione uguale, operando uguale
+    
+    c = 1 - a;
+    d = 1 - c; // test: d = 1 - 1 + a
+    b = d + 5;
+
+    // MUL - DIV
+    c = a * 3;
+    d = c / 3;
+    b = d + 5; // test: d = a * 3 / 3 = a
+
+    // MUL (commutative) - DIV // FUNZIONA (istruzione opposta, stesso operando numerico)
+    c = 3 * a;
+    d = c / 3;
+    b = d + 5; // test: d = a * 3 / 3 = a
+    
+    // --- Number at the FIRST operand (2° instruction)---
+    // istruzione è la stessa, primo operando è opposto
+    c = 1 + a;
+    d = -1 + c;
+    b = d + 5; // test: d = -1 + 1 + a = a
+    
+    // SUB - SUB
+    c = -1 - a;
+    d = -1 - c; 
+    b = d + 5; // test: d = -1 - (-1 - a) = -1 + 1 + a = a
+
+    // SUB - ADD (commutative) 
+    c = a - 1;
+    d = 1 + c;
+    b = d + 5; // test: d = a - 1 + 1 = a
+
+    // Tutte le precedenti devono funzionare
+    
+    // --- DON'T NEED OPTIMIZATION
+    c = 1 + a; 
+    d = 1 - c; // sottr. operando sx
+    b = d + 5;
+
+    // Non deve funzionare
+    c = -1 - a; // sottr. operando sx  (ad es. a - (-1) funzionerebbe)
+    d = -1 + c; // add
+    d = d + 5;
 
     return b;
 }
