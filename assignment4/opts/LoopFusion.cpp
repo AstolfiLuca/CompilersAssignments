5673c8b7580b8ffa0eb26d86726b3bbfe88b69cd
crisalioto <315179@studenti.unimore.it>
2025-05-14 15:58:21 +0200

    punto 1 e 3

--- assignment4/opts/LoopFusion.cpp
+++ assignment4/opts/LoopFusion.cpp
@@ -12,15 +12,17 @@
 
 int loop_counter = 1;
 
+// Di base: do_while sono non guarded, for sono guarded
 bool isAdjacentLoops(Loop *L1, Loop *L2){
-  BasicBlock *exitBlockL1 = L1->getExitBlock();
   if (L1->isGuarded()) {
     outs() << "Loop " << loop_counter << " is guarded\n";
-    if (exitBlockL1 == L2->getHeader())        // Exit Block di L1 è uguale all'header di L2
+    BasicBlock *successor = L1->getLoopGuardBranch()->getSuccessor(0);
+    if (L1->contains(successor)) successor = L1->getLoopGuardBranch()->getSuccessor(1); // ha sempre due successori: l'header e l'exit block del loop
+    if (successor == L2->getHeader()) // Il successore del guard branch di L1 è l'header di L2
       return true;
   } else {
     outs() << "Loop " << loop_counter << " is not guarded\n";
-    if(exitBlockL1 == L2->getLoopPreheader())  // Exit block di L1 è uguale al preheader di L2
+    if(L1->getExitBlock() == L2->getLoopPreheader())                  // L'exit block di L1 è il preheader di L2
       return true;
   }
 
@@ -51,12 +53,21 @@ bool isControlFlowEquivalence(Loop *L1, Loop *L2, DominatorTree &DT, PostDominat
 
 PreservedAnalyses LoopFusionPass::run(Function &F, FunctionAnalysisManager &AM) {
   LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
+  DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
+  PostDominatorTree &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
 
   // L1 è il loop attualmente analizzato, L2 è il loop successivo a L1
   // I loop vengono visitati al contrario
   for (auto L1 = LI.rbegin(), L2 = std::next(L1); L2 != LI.rend(); ++L1, ++L2) {
+    // Punto 1
     bool isA = isAdjacentLoops(*L1, *L2); // Dereferenzia gli iteratori per ottenere Loop*
     outs() << "-> Loop " << loop_counter << " adjacent: " << isA << "\n";
+    
+    // Punto 3
+    bool isCFE = isControlFlowEquivalence(*L1, *L2, DT, PDT);
+
+
+
     loop_counter++;
   }
   
