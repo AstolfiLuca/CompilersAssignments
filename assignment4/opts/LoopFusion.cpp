1c41f8ba6f8979aff47c46a9a909b1c2c64fd969
crisalioto <315179@studenti.unimore.it>
2025-05-14 15:14:52 +0200

    opt

--- assignment4/opts/LoopFusion.cpp
+++ assignment4/opts/LoopFusion.cpp
@@ -9,34 +9,19 @@
 #include "llvm/IR/Dominators.h"
 #include "llvm/ADT/SmallVector.h"
 
+using namespace llvm;
+
+int loop_counter = 1;
+
 bool isAdjacentLoops(Loop *L1, Loop *L2){
-  outs() << "\nProcessing BasicBlock from L1: ";
-  for (BasicBlock *BB1 : L1->blocks()) {
-    outs() << "\n";
-    BB1->printAsOperand(outs(), false);
-  }
-  
-  outs() << "\nProcessing BasicBlock from L2: ";
-  for (BasicBlock *BB2 : L2->blocks()) {
-    outs() << "\n";
-    BB2->printAsOperand(outs(), false);
-  }
-
   BasicBlock *exitBlockL1 = L1->getExitBlock();
-  if(L2->isGuarded()) outs() << "\nL2 is guarded\n";
-  else outs() << "\nL2 is not guarded\n";
-
-  if(L1->isGuarded()){
-    outs() << "\nL1 is guarded\n";
-     
-    BasicBlock *HeaderBlockL2 = L2->getHeader();
-    if(exitBlockL1 == HeaderBlockL2)   // Exit block di L1 è uguale al header di L2
+  if (L1->isGuarded()) {
+    outs() << "Loop " << loop_counter << " is guarded\n";
+    if (exitBlockL1 == L2->getHeader())        // Exit Block di L1 è uguale all'header di L2
       return true;
-  }
-  else{
-    outs() << "\nL1 is not guarded\n";
-    BasicBlock *preHeaderBlockL2 = L2->getLoopPreheader();
-    if(exitBlockL1 == preHeaderBlockL2)   // Exit block di L1 è uguale al preheader di L2
+  } else {
+    outs() << "Loop " << loop_counter << " is not guarded\n";
+    if(exitBlockL1 == L2->getLoopPreheader())  // Exit block di L1 è uguale al preheader di L2
       return true;
   }
 
@@ -45,23 +30,19 @@ bool isAdjacentLoops(Loop *L1, Loop *L2){
 
 PreservedAnalyses LoopFusionPass::run(Function &F, FunctionAnalysisManager &AM) {
   LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
-  
-  for (auto it = LI.rbegin(); it != LI.rend(); ++it) {
-    Loop *L1 = *it;
-    auto nextIt = std::next(it);
-    if (nextIt == LI.rend()){
-      outs() << "No more loops to process.\n";
-      break;
-    }
 
-    Loop *L2 = *nextIt;
-    outs() << "Processing loops: ";
+  // L1 è il loop attualmente analizzato, L2 è il loop successivo a L1
+  // I loop vengono visitati al contrario
+  for (auto L1 = LI.rbegin(), L2 = std::next(L1); L2 != LI.rend(); ++L1, ++L2) {
+    bool isA = isAdjacentLoops(*L1, *L2); // Dereferenzia gli iteratori per ottenere Loop*
+    outs() << "-> Loop " << loop_counter << " adjacent: " << isA << "\n";
 
-    bool isA = isAdjacentLoops(L1, L2);
-    outs() << " Loop adjacet: " << isA << "\n";
 
-    // Process L1 and L2
+
+
+    
+    loop_counter++;
   }
-
+  
   return PreservedAnalyses::all();
 }
